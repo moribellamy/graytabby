8e935a9627c70b903da1b6abec189cf55d7c71c6
Mori Bellamy <mori@invoked.net>
2019-11-07 21:36:18 -0800

    lint as part of travis

--- src/brokers.ts
+++ src/brokers.ts
@@ -25,7 +25,7 @@ class Broker<MessageT> {
     await browser.runtime.sendMessage(payload);
   }
 
-  public sub(func: (msg: MessageT, sender: any) => void): void {
+  public sub(func: (msg: MessageT, sender: void) => void): void {
     // eslint-disable-next-line
     const handler = (payload: Payload<MessageT>, sender: any) => {
       if (payload.type === this.key) {
